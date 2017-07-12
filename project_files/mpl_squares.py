7e733076e20715b08bdebe48760bedc1116a7406
domdew23 <ddewhurs@oswego.edu>
2017-07-12 14:51:26 -0400

    added mpl_squares.py

---
+++ project_files/mpl_squares.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+
+input_values = list(range(1,6))
+squares = [x**2 for x in range(1,6)]
+plt.plot(input_values, squares, linewidth=5)
+
+# Set the chart title and label axis
+plt.title("Square Numbers", fontsize=24)
+plt.xlabel("Value", fontsize=14)
+plt.ylabel("Square of Value", fontsize=14)
+
+# Set size of tick labels
+plt.tick_params(axis='both', labelsize=14)
+plt.show()
